677e3af497b8ab8e6ecb6c42edee97939cc47bfe
Lars Johan Folde <ljfolde@gmail.com>
2024-07-16 21:05:26 +0200

    Initialized git

---
+++ main.py
@@ -0,0 +1,74 @@
+from dataclasses import dataclass
+import sys
+import requests
+import pandas as pd
+
+@dataclass
+class Person:
+    name: str
+    average: float
+
+    def to_csv(self) -> str:
+        average = str(self.average).replace(".", ",")
+        return f'{self.name},"{average}"'
+
+def get_competitors(competition_id: str) -> list[str]:
+    url: str = f"https://worldcubeassociation.org/api/v0/competitions/{competition_id}/wcif/public"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print(f"Error status: {response.status_code}")
+        exit()
+
+    data = response.json()
+    competitors = []
+
+    for person in data['persons']:
+        # Splitting name to account for non-latin names
+        name = person['name'].split(" (")[0]
+        competitors.append(name)
+
+    return competitors
+
+def parse_csv(file: str) -> dict:
+    data: pd.DataFrame = pd.read_csv(file)
+    competitors: dict = {}
+    names: list[str] = [name for name in data['name']]
+    average: list[str] = [average.replace("\"", "") for average in data['average']]
+    for i in range(len(names)):
+        competitors.update({names[i]: average[i]})
+    return competitors
+
+def time_to_float(time: str) -> float:
+    time = time.replace(",", ".")
+    if ":" not in time:
+        return float(time)
+    split_time: list[str] = time.split(":")
+    minutes: int = int(split_time[0])
+    seconds: float = float(split_time[1])
+    return seconds + minutes * 60
+
+def data_to_csv(persons: list[list]) -> None:
+    competition_id: str = sys.argv[1]
+    output_file: str = f"psychsheet_{competition_id}.csv"
+    with open(output_file, 'w') as file:
+        file.write("name,average\n")
+        for person in persons:
+            file.write(f"{person.to_csv()}\n")
+
+def main() -> None:
+    output: list[Person] = []
+    competition_id: str = sys.argv[1]
+    competitors: list[str] = get_competitors(competition_id)
+    ranks: dict = parse_csv("fto_ranks.csv")
+
+    for competitor in competitors:
+        if competitor in ranks:
+            person = Person(competitor, time_to_float(ranks[competitor]))
+            output.append(person)
+
+    output = sorted(output, key=lambda x: x.average)
+    data_to_csv(output)
+
+if __name__ == "__main__":
+    main()
